c555a4f4466b2d337382848bf8a3e127870ebdfe
pro-grammer-SD <geniussantu1983@gmail.com>
2025-11-05 11:59:26 +0530

    up

--- app.py
+++ app.py
@@ -13,9 +13,11 @@ model = YOLO("yolov8n.pt")
 frame = camera_input_live()
 
 if frame is not None:
-    img = Image.open(frame)
-    img = np.array(img)
-    results = model.predict(img, conf=0.5, verbose=False)
+    img = Image.open(frame).convert("RGB")
+    np_img = np.array(img)
+    st.image(np_img, caption="📸 Live Camera Feed", use_container_width=True)
+
+    results = model.predict(np_img, conf=0.5, verbose=False)
     annotated = results[0].plot()
-    st.image(annotated, channels="BGR", use_container_width=True)
+    st.image(annotated, channels="BGR", caption="🎯 Detected Objects", use_container_width=True)
     
